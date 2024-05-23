5f4b0435f33e8ff363e7e924ff107b790ff823e3
Murilo Barros <murilobarros304@gmail.com>
2024-05-23 15:31:39 -0300

    testes

--- tello_zune.py
+++ tello_zune.py
@@ -90,10 +90,12 @@ class TelloZune(Tello):
 
         if self.video_decision == 'w' or self.video_decision == 'b':
             self.start_webcam_video()
-            cv2.namedWindow('Webcam')
+            #cv2.namedWindow('Webcam')
+            #cv2.waitKey(20)
             self.calc_fps(self.webcam_frame)
             self.frame_detection = self.webcam_frame
             cv2.imshow('Webcam', self.webcam_frame)
+            #cv2.waitKey(20)
             print('teste') # não ocorre
 
 
