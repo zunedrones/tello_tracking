9b1bf5df2943848a1e7f0dd6b28b3da0e328bc98
Murilo Barros <murilobarros304@gmail.com>
2024-06-03 10:13:53 -0300

    decoder

---
+++ h264decoder/build/CMakeFiles/h264decoder.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/murilo/Documentos/Tello-Python/Tello_Video/h264decoder/h264decoder.cpp" "CMakeFiles/h264decoder.dir/h264decoder.cpp.o" "gcc" "CMakeFiles/h264decoder.dir/h264decoder.cpp.o.d"
+  "/home/murilo/Documentos/Tello-Python/Tello_Video/h264decoder/h264decoder_python.cpp" "CMakeFiles/h264decoder.dir/h264decoder_python.cpp.o" "gcc" "CMakeFiles/h264decoder.dir/h264decoder_python.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
